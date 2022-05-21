a7e97d551551968bc28c20a599761b90245721e3
youngkwon02 <youngkwon02@naver.com>
2022-05-22 03:45:35 +0900

    feat: RecordController getRecords

---
+++ src/controllers/RecordController.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+
+import config from "../config";
+import categoryList from "../modules/categoryList";
+import message from "../modules/responseMessage";
+import statusCode from "../modules/statusCode";
+import util from "../modules/util";
+import { RecordService } from "../services";
+
+/**
+ *  @route GET /record
+ *  @desc Get Records
+ *  @access Public
+ */
+const getRecords = async (req: Request, res: Response) => {
+  const { category } = req.query;
+
+  // @error. category가 유효하지 않은 경우
+  if (typeof category !== "string" || !categoryList.includes(category)) {
+    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.INVALID_CATEGORY));
+  }
+
+  try {
+    const data = await RecordService.getRecords(config.defaultUserId, category);
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, message.GET_RECORDS_SUCCESS, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+};
+
+export default {
+  getRecords,
+};

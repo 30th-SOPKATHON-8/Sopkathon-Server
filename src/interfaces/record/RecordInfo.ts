8ade712b0d073fd1a1b04af1176c440c0755b200
youngkwon02 <youngkwon02@naver.com>
2022-05-22 00:05:40 +0900

    refactor: add title at RecordInfo interface

--- src/interfaces/record/RecordInfo.ts
+++ src/interfaces/record/RecordInfo.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export interface RecordInfo {
   price: number;
   isXibal: boolean;
+  title: string;
   content: string;
   createdAt: Date;
   userId: mongoose.Types.ObjectId;

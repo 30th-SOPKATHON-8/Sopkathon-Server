c13f5dd6a760dca7362caa2cf6753e1645fbbd04
soryeongk <soryeongk.kr@gmail.com>
2022-05-22 06:07:21 +0900

    fix: skip request argument when no used

--- src/controllers/RecordController.ts
+++ src/controllers/RecordController.ts
@@ -34,8 +34,7 @@ const getRecords = async (req: Request, res: Response) => {
   }
 };
 
-const getTotalRecord = async (req: Request, res: Response): Promise<void> => {
-  console.log(req);
+const getTotalRecord = async (_: undefined, res: Response): Promise<void> => {
   try {
     const data: GetTotalRecordDto = await RecordService.getTotalRecord(config.defaultUserId);
 

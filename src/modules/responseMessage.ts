a623b236d4141539c9b81cbf4b658e810be9e67f
youngkwon02 <youngkwon02@naver.com>
2022-05-22 02:03:31 +0900

    chore: update modules/responseMessage

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -6,19 +6,8 @@ const message = {
   BAD_REQUEST: "잘못된 요청",
   INTERNAL_SERVER_ERROR: "서버 내부 오류",
 
-  // 유저
-  READ_USER_SUCCESS: "유저 조회 성공",
-  CREATE_USER_SUCCESS: "유저 생성 성공",
-
-  // 주문
-  CREATE_ORDER_SUCCESS: "리뷰 작성 성공",
-  READ_ORDER_SUCCESS: "리뷰 조회 성공",
-
-  // 문의
-  CREATE_INQUIRY_SUCCESS: "문의 생성 성공",
-  CREATE_INQUIRY_COMMENT_SUCCESS: "문의 작성 성공",
-  READ_INQUIRY_SUCCESS: "문의 조회 성공",
-  DELETE_INQUIRY_SUCCESS: "문의 삭제 성공",
+  // Record
+  INVALID_CATEGORY: "올바르지 않은 category 값 입니다.",
 };
 
 export default message;

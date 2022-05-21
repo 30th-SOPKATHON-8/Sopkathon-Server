a5f99b71849777c99ce0288960d2d2d058362f2a
youngkwon02 <youngkwon02@naver.com>
2022-05-22 00:13:09 +0900

    feat: define Record models

---
+++ src/models/Record.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+import { RecordInfo } from "../interfaces/record/RecordInfo";
+
+const recordSchema = new mongoose.Schema(
+  {
+    price: {
+      type: Number,
+      required: true,
+    },
+    isXibal: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model<RecordInfo & mongoose.Document>("Record", recordSchema);

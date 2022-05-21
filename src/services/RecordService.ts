c66df4844818bdee371b13a8848d7663a9488c00
youngkwon02 <youngkwon02@naver.com>
2022-05-22 03:45:24 +0900

    refactor: RecordService getRecords function

--- src/services/RecordService.ts
+++ src/services/RecordService.ts
@@ -16,14 +16,15 @@ const getRecords = async (userId: string, category: string): Promise<RecordRespo
       filter = { userId, isXibal: category === "bad" };
     }
 
-    const records = await Record.find(filter).populate("userId");
+    const records = await Record.find(filter).populate("userId").sort("-createdAt");
 
     const data = records.map((r: any): RecordResponseDto => {
-      const price = r.isXibal ? "+ " : "- " + r.price.toLocaleString();
+      const price = (r.isXibal ? "+" : "-") + r.price.toLocaleString() + " 시발코인";
       const createDay = dayjs(r.createdAt);
+
       const createdAt = `${createDay.month() + 1}월 ${
         createDay.date() + 1
-      }일 ${createDay.hour()}:${createDay.minute()}${createDay.hour() + 1 < 12 ? "am" : "pm"}`;
+      }일 ${createDay.hour()}:${createDay.minute()}`;
 
       const result = {
         _id: r._id,
